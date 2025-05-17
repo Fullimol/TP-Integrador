915bf437d30d36e943dc3633cad4b8d8aa625087
Bruno Freijomil <bruno_freijomil@hotmail.com>
2025-05-17 17:20:39 -0300

    se pide nombre del usuario

--- frontend/scripts/cliente/bienvenida.js
+++ frontend/scripts/cliente/bienvenida.js
@@ -1,1 +1,13 @@
 /* Logica de la pagina de bienvenida */
+
+/* obtener nombre y guardarlo en el localStorage. Luego ir a productos.html */
+
+const formBienvenida = document.getElementById('form-bienvenida');
+const nombreBienvenida = document.getElementById('nombre-bienvenida');
+
+formBienvenida.addEventListener('submit', (e) => {
+    e.preventDefault();
+    localStorage.setItem('nombre', nombreBienvenida.value);
+    window.location.href = 'productos.html';
+});
+
--- frontend/scripts/cliente/productos.js
+++ frontend/scripts/cliente/productos.js
@@ -1,1 +1,4 @@
 /* Logica de la pagina donde se muestran los productos */
+
+// obtener nombre del localStorage
+console.log(localStorage.getItem('nombre'));
