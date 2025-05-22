093f13877ac3316951b9694e279177b6960969f2
Bruno Freijomil <bruno_freijomil@hotmail.com>
2025-05-22 15:33:29 -0300

    pequeños fixs

--- frontend/scripts/utils.js
+++ frontend/scripts/utils.js
@@ -4,16 +4,7 @@ Se usa típicamente para funciones reutilizables o herramientas auxiliares que n
 - Validación de formularios.
 - Funciones matemáticas comunes.
 - Manipulación de strings.
-
 */
-
-// al dar click en "Salir" borro el nombre del localStorage
-const btnSalir = document.querySelector('.salir-button');
-btnSalir.addEventListener('click', () => {
-    localStorage.removeItem('nombre');
-});
-
-
 
 /* MODO OSCURO */
 const btnDarkMode = document.getElementById("btn-darkMode");
