a457aac0eb850dd51b00af0f2b7b99a13b187673
Bruno Freijomil <bruno_freijomil@hotmail.com>
2025-05-22 15:12:24 -0300

    se agrega verificacion nombre vacio

--- frontend/scripts/cliente/bienvenida.js
+++ frontend/scripts/cliente/bienvenida.js
@@ -4,6 +4,7 @@
 
 const formBienvenida = document.getElementById('form-bienvenida');
 const nombreBienvenida = document.getElementById('nombre-bienvenida');
+const btnBienvenida = document.getElementById('btn-bienvenida');
 
 
 // limpio todo el localStorage cada vez que cargo la pag bienvenida:
@@ -15,3 +16,16 @@ formBienvenida.addEventListener('submit', (e) => {
     window.location.href = 'productos.html';
 });
 
+// vericar que el input NO esté vacio:
+
+function verificarInputNombre() {
+    const nombre = nombreBienvenida.value.trim();
+
+    if (nombre.length < 3) {
+        btnBienvenida.classList.add('deshabilitarButton');
+    } else {
+        btnBienvenida.classList.remove('deshabilitarButton');
+    }
+}
+
+nombreBienvenida.addEventListener('input', verificarInputNombre);
