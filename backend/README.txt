ab7e20db2b6bc3e955fe985c7d6226f68a6330f1
Bruno Freijomil <bruno_freijomil@hotmail.com>
2025-05-24 12:29:16 -0300

    se agregan los ejs

---
+++ backend/README.txt
@@ -0,0 +1,9 @@
+Paquetes usados:
+
+npm install express
+npm install cors
+npm install mysql2
+npm install -g nodemon
+npm install ejs
+
+Usar en consola sobre la carpeta backend "nodemon api.js" para levantar servidor.
--- backend/api.js
+++ backend/api.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
-const juegosRoutes = require('./routes/juegosRutas');
+const path = require('path');
 
+// configuro el EJS como motor de vistas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
-var cors = require('cors');
-app.use(cors());//estoy seteando cors como middleware para todas las solicitudes
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
+// Rutas
+const juegosRutas = require('./routes/juegosRutas');
+app.use('/juegos', juegosRutas); // incluye rutas de vistas y lógica relacionada
 
-
-// ruta juegos
-app.use('/juegos', juegosRoutes);
-
-
-
-app.listen(3000, () => console.log('API escuchando en http://localhost:3000'));
+// Escuchar
+app.listen(3000, () => console.log('Servidor escuchando en http://localhost:3000'));
